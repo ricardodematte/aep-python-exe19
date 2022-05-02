c9bd69de73c56638adf29d83e3c46d6afb95d31b
Ricardo Dematte <103331571+ricardodematte@users.noreply.github.com>
2022-05-02 19:11:51 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+# Mostrar a multiplicação de dois números inteiros
+
+import os
+n1 = (int(input("Digite um número inteiro n1:\n")))
+n2 = (int(input("Digite um número inteiro n2:\n")))
+mult = n1*n2
+os.system("clear")
+print ("A multiplicação é ",mult)
